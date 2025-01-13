559888b08b812aac522153ef29d41e58c5217798
Percy Itzstarks QC <percy91@gmail.com>
2025-01-13 17:30:22 -0500

    insertar imagenes funcionable
    falta 1 detalle

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const http = require('http');
 const socket = require('socket.io');
@@ -5,6 +6,9 @@ const socket = require('socket.io');
 const app = express();
 const httpServer = http.createServer(app);
 const io = socket(httpServer);
+
+const apiKeyIa = process.env.API_KEY_IA
+console.log('apiKeyIa',apiKeyIa)
 
 app.use(express.static('public'));
 
@@ -29,8 +33,48 @@ io.on('connection', async (socket)=>{
         io.emit('evt_usuarioMensaje', {
             ...mensaje,
             fechaHora: new Date(),
-            idSocketRemitente: id
+            idSocketRemitente: id,
+            isIa: false, 
+            message: mensaje.message || '',
+            image: mensaje.image
         });
+        if(mensaje.message.startsWith('@sisegpt')){
+            const url = "https://api-inference.huggingface.co/models/mistralai/Mistral-Nemo-Instruct-2407";
+            const body = {
+                            inputs: mensaje.message.replace('@sisegpt',''),
+                            options: {
+                                wait_for_model: true
+                            }
+                        };
+            console.log('fetch',url, body);
+            try {
+                const response = await fetch(url,{
+                    method: "POST",
+                    headers: {
+                        'Authorization': 'Bearer '+apiKeyIa,
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(body)
+                });
+                if (!response.ok) {
+                    throw new Error(`Response status: ${response.status}`);
+                }
+                const json = await response.json();
+                console.log('Respuesta IA',json);
+                if(json && json.length > 0){
+                    io.emit('evt_usuarioMensaje', {
+                        message: json[0].generated_text,
+                        nickname: 'Sise GPT',
+                        fechaHora: new Date(),
+                        idSocketRemitente: 'ABC-1234-DEF-IA-SISEGPT',
+                        isIa: true
+                    });
+                }
+                
+            } catch (error) {
+                console.error(error.message);
+            }
+        }        
     });
 
     //RECIBO LA UBICACION DEL USUARIO X
